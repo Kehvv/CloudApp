e105acd23bbbe2d07e6e90b7d733511c19f50b79
Kevin <kevinpmbyrne@gmail.com>
2020-04-25 20:17:32 +0100

    Added crypto news page and API

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,13 +2,12 @@ class HomeController < ApplicationController
   def index
   	require 'net/http'
   	require 'json'
-  	
+
+  	@url = 'https://min-api.cryptocompare.com/data/v2/news/?lang=EN&api_key={73700d20cab257ca1696d80644bb6430f8857b022480989a3a3ddb625505b6c3}'
+    @uri = URI(@url)
+    @response = Net::HTTP.get(@uri)
+    @news = JSON.parse(@response)
   
-  	@url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=cce65e50-1c57-4d55-a23d-4d5744c6a1e8&start=1&limit=5000&convert=USD'
-  	@uri = URI(@url)
-  	@response = Net::HTTP.get(@uri)
-  	@coins = JSON.parse(@response)
-  	@my_coins = ["BTC", "XRP", "ADA", "XLM", "STEEM",]
 
 	
   end
